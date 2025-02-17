7714d78c7b915a98e6acd7172831fd901ecf8dc9
Stepan <shmatok.stepan@mail.ru>
2025-02-17 23:36:32 +0300

    “fixes_to_improve”

--- HW7.py
+++ HW7.py
@@ -9,18 +9,9 @@ currentmin2 = motomin2 % 60
 # print(f'{currenthours1:02d}:{currentmin1:02d}')
 # print(f'{currenthours2:02d}:{currentmin2:02d}')
 
-numb1hour1 = currenthours1 // 10
-numb1hour2 = currenthours1 % 10
-numb1min1 = currentmin1 // 10
-numb1min2 = currentmin1 % 10
+numbsum1 = (currenthours1 // 10) + (currenthours1 % 10) + (currentmin1 // 10) + (currentmin1 % 10)
+numbsum2 = (currenthours2 // 10) + (currenthours2 % 10) + (currentmin2 // 10) + (currentmin2 % 10)
 
-numb2hour1 = currenthours2 // 10
-numb2hour2 = currenthours2 % 10
-numb2min1 = currentmin2 // 10
-numb2min2 = currentmin2 % 10
-
-numbsum1 = numb1hour1 + numb1hour2 + numb1min1 + numb1min2
-numbsum2 = numb2hour1 + numb2hour2 + numb2min1 + numb2min2
 print(numbsum1)
 print(numbsum2)
 
