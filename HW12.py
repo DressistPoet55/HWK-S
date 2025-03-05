96bbb9abf580ca23bad48090d28ec4ecf7273d28
Stepan <shmatok.stepan@mail.ru>
2025-03-05 17:07:33 +0300

    "Fixes_1"

--- HW12.py
+++ HW12.py
@@ -6,7 +6,6 @@ class Deposit:
         self.interest_rate = interest_rate
         self.koll_months = years * 12
 
-
     def calculate_final_amount(self):
         monthly_rate = self.interest_rate / 12
         final_amount = self.start_balance * (1 + monthly_rate) ** self.koll_months
@@ -18,29 +17,26 @@ class Bank:
         self.clients = {}
         self.deposits = {}
 
-
     def register_client(self, client_id, name):
         self.clients[client_id] = name
         return f'Зарегистрирован клиент по имени {name} , и присвоен личный id {client_id}'
 
-
     def open_deposit_account(self, client_id, start_balance, years):
         if client_id in self.clients:
             self.deposits[client_id] = Deposit(start_balance, years)
-            return f'Открыт депозитный счет по id: {client_id}, с балансом {start_balance} рублей на {years} лет'
+            return (f'Открыт депозитный счет по id: {client_id}, '
+                    f'с балансом {start_balance} рублей на {years} лет')
         else:
             return f'Клиент с ID {client_id} не найден'
-
 
     def calc_deposit_interest_rate(self, client_id):
         if client_id in self.deposits:
             return self.deposits[client_id].calculate_final_amount()
 
-
     def close_deposit(self, client_id):
         if client_id in self.deposits:
             del self.deposits[client_id]
-            return f'Депозитный счет закрыт'
+            return 'Депозитный счет закрыт'
 
 
 bank = Bank()
@@ -68,20 +64,17 @@ class Book:
         self.reserv = None
         self.givenbook = None
 
-
     def reserve(self, Reader):
         if self.reserv is None:
             self.reserv = Reader
             return f'Книга {self.book_name} зарезервирована за {Reader.name}'
         else:
-            return f'Книга уже заревервирована'
-
+            return 'Книга уже заревервирована'
 
     def cancel_reserve(self, Reader):
         if self.reserv == Reader:
             self.reserv = None
             return f'Резервация на книгу {self.book_name} отменена'
-
 
     def get_book(self, Reader):
         if self.givenbook is None and self.reserv == Reader:
@@ -91,33 +84,28 @@ class Book:
         elif self.givenbook is not None:
             return f'Книга {self.book_name} уже выдана другому пользователю'
         else:
-            return f'Вы не можете взять книгу, так как она не зарезервирована'
-
+            return 'Вы не можете взять книгу, так как она не зарезервирована'
 
     def return_book(self, Reader):
         if self.givenbook == Reader:
             self.givenbook = None
             return f'Книга {self.book_name} возвращена {Reader.name}'
         else:
-            return f'У вас нет книги'
+            return 'У вас нет книги'
 
 
 class Reader:
     def __init__(self, name):
         self.name = name
 
-
     def reserve_book(self, book):
         return book.reserve(self)
-
 
     def cancel_reserve(self, book):
         return book.cancel_reserve(self)
 
-
     def get_book(self, book):
         return book.get_book(self)
-
 
     def return_book(self, book):
         return book.return_book(self)
@@ -129,11 +117,11 @@ petya = Reader("Petya")
 
 
 print(vasya.reserve_book(book))
-print(petya.reserve_book(book)) #<-- User can not reserve a book
+print(petya.reserve_book(book))
 print(vasya.cancel_reserve(book))
 print(petya.reserve_book(book))
-print(vasya.get_book(book)) #<-- User can not get a book
+print(vasya.get_book(book))
 print(petya.get_book(book))
-print(vasya.return_book(book)) #<-- User can not return a book
+print(vasya.return_book(book))
 print(petya.return_book(book))
 print(vasya.get_book(book))
