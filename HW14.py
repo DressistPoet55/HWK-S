06ee79709e9925688842b53cb0189cb3a8bc428a
Stepan <shmatok.stepan@mail.ru>
2025-03-14 12:42:53 +0300

    'Fixes_7?'

--- HW14.py
+++ HW14.py
@@ -20,7 +20,8 @@ students = []
 with open('students.txt', 'r', encoding="utf-8") as file:
     for line in file:
         parts = line.strip().split(', ')
-        name, group = parts[0], parts[1]
+        name = parts[0]
+        group = parts[1]
         grades = parts[2:]
         grades = list(map(int, grades))
         students.append({'name': name, 'group': group, 'grades': grades})
