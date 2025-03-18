9819b73266ad633d29f424f5f242e1059992240e
Stepan <shmatok.stepan@mail.ru>
2025-03-18 19:18:39 +0300

    'Task1_compl'

---
+++ Test_taskss/Task1.py
@@ -0,0 +1,18 @@
+# Task 1
+strok = 'Это строка для первого задания в тесте'
+
+perv = strok[0]
+vtor = strok[-1]
+tri = strok[2]
+chetv = strok[-3]
+lenth_strok = len(strok)
+rev_strok = strok[::-1]
+vosem_simv = strok[0:8]
+
+print(perv)
+print(vtor)
+print(tri)
+print(chetv)
+print(lenth_strok)
+print(rev_strok)
+print(vosem_simv)
