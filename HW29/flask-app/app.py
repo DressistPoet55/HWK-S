8ecb3544e9c74a02cb9f1732a6297531bea12775
Stepan <shmatok.stepan@mail.ru>
2025-05-20 23:12:13 +0300

    linter_fixes

--- HW29/flask-app/app.py
+++ HW29/flask-app/app.py
@@ -9,10 +9,12 @@ images = [
     "https://media.tenor.com/lQlIBQeeruwAAAAd/wanted-cat.gif"
 ]
 
+
 @app.route('/')
 def index():
     url = random.choice(images)
     return render_template('index.html', url=url)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
